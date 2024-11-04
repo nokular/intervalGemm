2b08efabe4bf653b6079c509e437980300488c50
kilbraun <60895409+nokular@users.noreply.github.com>
2024-11-04 23:29:49 +0100

    Updated README

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # intervalGemm
+
+This is an implementation of a GEMM kernel for CUDA for interval arithmetic. An example use can be seen in example.cu
--- example.cu
+++ example.cu
@@ -1,11 +1,7 @@
 #include <stdio.h>
-
 #include <iostream>
-// include a timing library
-#include <cublas_v2.h>
 #include <cuda.h>
 #include <cuda_runtime.h>
-
 #include <algorithm>
 #include <cassert>
 #include <chrono>
@@ -38,15 +34,10 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  // initialize random generator
-  std::random_device rd;
-  std::mt19937 gen(rd());
-  std::uniform_real_distribution<float> dis(0.0, 1.0);
   float a = 1.3213 + 20;
   float b = 1.3213 + 20;
   float c = 0;
 
-  // create 2 interval matrices with random values of size 2x2
   int dimM = 2048;
   int dimN = 2048;
   int dimK = 2048;
@@ -113,20 +104,16 @@ int main(int argc, char **argv) {
   std::cout << "Avg diff: " << total_diff << std::endl;
   std::cout << t << std::endl;
   std::cout << "Block: " << BLOCKDIMX << " " << BLOCKDIMY << std::endl;
-  // dim3((N + BTILEN - 1) / BTILEN, (M + BTILEM - 1) / (BTILEM));
   std::cout << "Grid: " << (dimN + BTILEN - 1) / BTILEN << " "
             << (dimM + BTILEM - 1) / BTILEM << std::endl;
   uint m = A.rows;  // Height of matrix A
   uint n = B.cols;  // Width of matrix B
   uint k = A.cols;  //
-  // print all the TM, TN, WITERN, WITERM, 1, KTILE, BLOCKDIMX, BLOCKDIMY,
-  // WTILEN, WTILEM, BTILEM, BTILEN
   std::cout << "TM: " << TM << " TN: " << TN << " WITERN: " << WITERN
             << " WITERM: " << WITERM << " KTILE: " << KTILE
             << " BLOCKDIMX: " << BLOCKDIMX << " BLOCKDIMY: " << BLOCKDIMY
             << " WTILEN: " << WTILEN << " WTILEM: " << WTILEM
             << " BTILEM: " << BTILEM << " BTILEN: " << BTILEN << std::endl;
-  // print N,m,k
   std::cout << "N: " << n << " M: " << m << " K: " << k << std::endl;
   std::cout << "Total ULPs: " << total_ulps << std::endl;
   std::cout << "Average ULP difference: " << total_ulps / (dimM * dimN)
